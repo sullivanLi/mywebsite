cbb32516c7c5e3d6dc8cf76e516c13c8c6de3c12
Sullivan Lee <sullivan1982@gmail.com>
2016-12-28 17:23:40 -0800

    works section

--- _js/functions.js
+++ _js/functions.js
@@ -1,3 +1,8 @@
+$(function() {
+  // Handler for .ready() called.
+  show_block($('#box1'));
+});
+
 $('.mobile-toggle').on('click', function(){
   $('.mobile-toggle').toggleClass('is-open');
   $('header').toggleClass('is-open');
@@ -11,3 +16,45 @@ $('.home-sections').on('click', function(){
     $('.home-sections').removeClass('is-open');
   }
 });
+
+$('.circle').on('click', function(){
+  if(!$(this).hasClass('disabled')){
+    let $prepareBox = $('#'+$(this).data('boxid'));
+    let $showBox = $('.box .item.show').parent().parent();
+    hide_block($showBox);
+    setTimeout(function(){
+      show_block($prepareBox);
+    }, 100);
+    $(this).addClass('disabled');
+    $(this).siblings().removeClass('disabled');
+    $('.web').removeClass('preactive-left preactive-right active');
+    if($('.circle').first().hasClass('disabled')){
+      $('.web').first().addClass('active');
+      $('.web').last().addClass('preactive-right');
+    } else {
+      $('.web').first().addClass('preactive-left');
+      $('.web').last().addClass('active');
+    }
+  }
+});
+
+function show_block($box) {
+  $box.find('.item').each(function(i){
+    setTimeout(function(){
+      $box.find('.item').eq(i).addClass('show');
+    }, 50 * i);
+  });
+}
+
+function hide_block($box) {
+  let item_length = $box.find('.item').length;
+  $box.find('.item').each(function(i){
+    setTimeout(function(){
+      $box.find('.item').eq(i).addClass('hiding');
+      if(item_length === (i +1) ) {
+        //last animation and remove all class
+        $box.find('.item').removeClass('show hiding');
+      }
+    }, 50 * i);
+  });
+}
